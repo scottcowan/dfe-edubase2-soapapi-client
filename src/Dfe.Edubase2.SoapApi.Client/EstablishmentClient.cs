bd6c7cf9d91ca27a36123edaefa2df88f2bb4159
Scott Cowan <info@scottcowan.org>
2017-06-06 15:25:18 +0100

    Adding pre request and post request hooks

--- src/Dfe.Edubase2.SoapApi.Client/EstablishmentClient.cs
+++ src/Dfe.Edubase2.SoapApi.Client/EstablishmentClient.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Dfe.Edubase2.SoapApi.Client.EdubaseService;
 
 namespace Dfe.Edubase2.SoapApi.Client
 {
-    public class EstablishmentClient
+    public class EstablishmentClient : IEstablishmentClient
     {
         private readonly IEdubaseClient _client;
 
@@ -17,13 +18,29 @@ namespace Dfe.Edubase2.SoapApi.Client
             _client = client;
         }
 
+        /// <summary>
+        /// An action run before each query to allow it to be logged
+        /// </summary>
+        public Action<EstablishmentFilter> PreRequest { get; set; }
+
+        /// <summary>
+        /// An action run after a response has been received
+        /// </summary>
+        public Action<EstablishmentFilter, FindEstablishmentsResponse> PostRequestAsync { get; set; }
+
+        public Action<EstablishmentFilter, EstablishmentList> PostRequest { get; set; }
+
 
         public IEnumerable<Establishment> FindEstablishments(EstablishmentFilter filter = null)
         {
             int pages;
             filter = filter ?? new EstablishmentFilter();
             filter.Page = 0;
-            foreach (var establishment in _client.FindEstablishments(filter, out pages))
+            PreRequest?.Invoke(filter);
+            var response = _client.FindEstablishments(filter, out pages);
+            PostRequest?.Invoke(filter, response);
+
+            foreach (var establishment in response)
             {
                 yield return establishment;
             }
@@ -31,7 +48,12 @@ namespace Dfe.Edubase2.SoapApi.Client
             for (var i = 1; i < pages; i++)
             {
                 filter.Page = i;
-                foreach (var establishment in _client.FindEstablishments(filter, out pages))
+
+                PreRequest?.Invoke(filter);
+                response = _client.FindEstablishments(filter, out pages);
+                PostRequest?.Invoke(filter, response);
+
+                foreach (var establishment in response)
                 {
                     yield return establishment;
                 }
@@ -43,15 +65,24 @@ namespace Dfe.Edubase2.SoapApi.Client
             var list = new List<Establishment>();
             filter = filter ?? new EstablishmentFilter();
             filter.Page = 0;
-            var response = await _client.FindEstablishmentsAsync(new FindEstablishmentsRequest(filter));
+            var response = await RequestEstablishments(filter);
+
             list.AddRange(response.Establishments);
             for (var i = 1; i < response.PageCount; i++)
             {
                 filter.Page = i;
-                list.AddRange((await _client.FindEstablishmentsAsync(new FindEstablishmentsRequest(filter))).Establishments);
+                list.AddRange((await RequestEstablishments(filter)).Establishments);
             }
 
             return list;
+        }
+
+        private async Task<FindEstablishmentsResponse> RequestEstablishments(EstablishmentFilter filter)
+        {
+            PreRequest?.Invoke(filter);
+            var response = await _client.FindEstablishmentsAsync(new FindEstablishmentsRequest(filter));
+            PostRequestAsync?.Invoke(filter, response);
+            return response;
         }
 
         public Establishment GetEstablishment(long urn)
---
+++ src/Dfe.Edubase2.SoapApi.Client/IEstablishmentClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dfe.Edubase2.SoapApi.Client.EdubaseService;
+
+namespace Dfe.Edubase2.SoapApi.Client
+{
+    public interface IEstablishmentClient
+    {
+        /// <summary>
+        /// An action run before each query to allow it to be logged
+        /// </summary>
+        Action<EstablishmentFilter> PreRequest { get; set; }
+
+        /// <summary>
+        /// An action run after a response has been received
+        /// </summary>
+        Action<EstablishmentFilter, FindEstablishmentsResponse> PostRequestAsync { get; set; }
+
+        Action<EstablishmentFilter, EstablishmentList> PostRequest { get; set; }
+
+        IEnumerable<Establishment> FindEstablishments(EstablishmentFilter filter = null);
+        Task<List<Establishment>> FindEstablishmentsAsync(EstablishmentFilter filter = null);
+        Establishment GetEstablishment(long urn);
+        Task<Establishment> GetEstablishmentAsync(long urn);
+    }
+}
